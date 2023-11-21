3d2512a10266a26bb0fd6426818ed266e178f6f1
norppa <108995411+norpba@users.noreply.github.com>
2023-11-21 15:12:20 +0200

    Worked on the project; Program now gets the src dest files' creation / modification datetime info.

--- src/importOS_universal.py
+++ src/importOS_universal.py
@@ -1,9 +1,10 @@
 import os
 import shutil
+import time as t
 from pathlib import Path
 
 # get the source directory from user input
-src_directory = str(input("Source directory: "))
+src_directory = 'C:\\Users\\norppa\\nerdCorner\\input for sorter' #str(input("Source directory: "))
 src = Path(src_directory).expanduser()
 
 # use os.path.expanduser() to handle ~ in the path
@@ -17,4 +18,16 @@ if not os.path.exists(dst_directory):
 
 # copy the contents of the source directory directly into the destination directory
 for item in src.glob('*'):
+
+    # get the creation and modification datetime of the file
+    creation_time = os.path.getctime(item)
+    modification_time = os.path.getmtime(item)
+
+    # convert the timestamps to datetime objects
+    creation_datetime = t.ctime(creation_time)
+    modification_datetime = t.ctime(modification_time)
+
+    print(creation_datetime)
+    print(modification_datetime)
+
     shutil.copy2(item, dst_directory)
