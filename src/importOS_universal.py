a6fba8d2c5c3bef6db3564b027f99ed5a7bc9740
norppa <108995411+norpba@users.noreply.github.com>
2023-11-21 22:31:21 +0200

    Worked on the project; program now creates folders and dynamically sorts files into the correct folders. Next step is to modify the code and add src/dst into buttons on the GUI.

--- src/importOS_universal.py
+++ src/importOS_universal.py
@@ -29,21 +29,15 @@ for item in src.glob('*'):
 
     # create a variable to hold the creation year of the current item
     # (currently works for windows only)
-    sorted_dir_name = modification_datetime[len(modification_datetime)-4:]
+    year_dir_name = modification_datetime[len(modification_datetime)-4:]
 
     # create a full path for the new dir
-    new_dir = os.path.join(dst_directory, sorted_dir_name)
+    new_dir = os.path.join(dst_directory, year_dir_name)
     
     # check if the directory already exists
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
+    
+    destination_file_path = os.path.join(new_dir, item.name)
 
-for item in src.glob('*'):
-
-    # get the creation and modification datetime of the file
-    creation_time = os.path.getctime(item)
-    modification_time = os.path.getmtime(item)
-
-    # convert the timestamps to datetime objects
-    creation_datetime = t.ctime(creation_time)
-    modification_datetime = t.ctime(modification_time)
+    shutil.copy2(item, destination_file_path)
