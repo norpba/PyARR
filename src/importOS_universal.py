f28b0a6ea0a71a79daf086591b59fc1e81ac98b6
norppa <108995411+norpba@users.noreply.github.com>
2023-11-21 15:03:12 +0200

    Deleted old importOS.py -source file and worked on new one to work universally on macOS and windows.

--- src/importOS_universal.py
+++ src/importOS_universal.py
@@ -2,18 +2,19 @@ import os
 import shutil
 from pathlib import Path
 
-# Use os.path.expanduser() to handle ~ in the path
-dst_directory = str(input("Destination directory: "))
-dst_directory = os.path.expanduser(dst_directory)
-
-# Check if the destination directory exists before creating it
-if not os.path.exists(dst_directory):
-    os.makedirs(dst_directory)
-
-# Get the source directory from user input
+# get the source directory from user input
 src_directory = str(input("Source directory: "))
 src = Path(src_directory).expanduser()
 
-# Copy the contents of the source directory directly into the destination directory
+# use os.path.expanduser() to handle ~ in the path
+dst_directory = str(input("Destination directory: "))
+dst_directory = os.path.expanduser(dst_directory)
+
+# check if the destination directory exists before creating it
+if not os.path.exists(dst_directory):
+    dstdir_name = str(input("Name the folder you want the files to be copied to: "))
+    os.makedirs(dst_directory)
+
+# copy the contents of the source directory directly into the destination directory
 for item in src.glob('*'):
     shutil.copy2(item, dst_directory)
