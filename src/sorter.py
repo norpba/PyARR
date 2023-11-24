6111ef59e2bafc69bf267598ad6c8665e89261c9
norppa <108995411+norpba@users.noreply.github.com>
2023-11-24 18:47:31 +0200

    A lot of changes. Changed project structure, 'tkinter_first_gui.py' now calls 'sorter.py' and uses the sorting logic inside it.

--- src/sorter.py
+++ src/sorter.py
@@ -3,41 +3,38 @@ import shutil
 import time as t
 from pathlib import Path
 
-# get the source directory from user input
-src_directory = str(input("Source directory: "))
-src = Path(src_directory).expanduser()
+def sort_files(src_directory, dst_directory):
+    # use os.path.expanduser() to handle ~ in the path
+    src = Path(src_directory).expanduser()
+    dst_directory = os.path.expanduser(dst_directory)
 
-# use os.path.expanduser() to handle ~ in the path
-dst_directory = str(input("Destination directory: "))
-dst_directory = os.path.expanduser(dst_directory)
+    # check if the destination directory exists before creating it
+    if not os.path.exists(dst_directory):
+        os.makedirs(dst_directory)
 
-# check if the destination directory exists before creating it
-if not os.path.exists(dst_directory):
-    os.makedirs(dst_directory)
+    # create folders that are named after the file modification year and copy files from
+    # the source directory into the newly created folders based on the file modification year
+    for item in src.glob('*'):
 
-# create folders that are named after the file modification year and copy files from
-# the source directory into the newly created folders based on the file modification year
-for item in src.glob('*'):
+        # get the creation and modification datetime of the file
+        creation_time = os.path.getctime(item)
+        modification_time = os.path.getmtime(item)
 
-    # get the creation and modification datetime of the file
-    creation_time = os.path.getctime(item)
-    modification_time = os.path.getmtime(item)
+        # convert the timestamps to datetime objects
+        creation_datetime = t.ctime(creation_time)
+        modification_datetime = t.ctime(modification_time)
 
-    # convert the timestamps to datetime objects
-    creation_datetime = t.ctime(creation_time)
-    modification_datetime = t.ctime(modification_time)
+        # create a variable to hold the creation year of the current item
+        # seems to be working for both macOS and windows right now
+        year_dir_name = modification_datetime[len(modification_datetime) - 4:]
 
-    # create a variable to hold the creation year of the current item
-    # seems to be working for both macos and windows right now
-    year_dir_name = modification_datetime[len(modification_datetime)-4:]
+        # create a full path for the new dir
+        new_dir = os.path.join(dst_directory, year_dir_name)
 
-    # create a full path for the new dir
-    new_dir = os.path.join(dst_directory, year_dir_name)
-    
-    # check if the directory already exists
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
-    
-    destination_file_path = os.path.join(new_dir, item.name)
+        # check if the directory already exists
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
 
-    shutil.copy2(item, destination_file_path)
+        destination_file_path = os.path.join(new_dir, item.name)
+
+        shutil.copy2(item, destination_file_path)
---
+++ src/tkinter_first_gui.py
@@ -0,0 +1,96 @@
+
+# gui for PyARR
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from sorter import sort_files
+
+
+def select_source_directory():
+    src_directory = filedialog.askdirectory()
+    # call the file sorting logic with the selected source directory
+    sort_files(src_directory, dst_directory)
+
+def select_destination_directory():
+    dst_directory = filedialog.askdirectory()
+
+class WelcomeWindow(Toplevel):
+
+    def __init__(self, root, *args, **kwargs):
+        super().__init__(root, *args, **kwargs)
+        self.iconbitmap("C:/Users/norppa/nerdCorner/PyARR/src/titlebar_icon.ico")
+        self.title("Welcome!")
+        self.configure(bg="snow")
+        info4 = ttk.Label(self, text="Welcome to Python Arranger!")
+        info4.pack()
+        b1 = ttk.Button(self, text="Close", command=self.destroy)
+        b1.pack()
+
+        # call the function center_window
+        self.center_window()
+        self.resizable(False, False)
+
+        # making a function to ensure that the window opens in the middle of the screen
+    def center_window(self):
+        w = 300 # width of the welcome window
+        h = 100 # height of the welcome window
+
+            # get the screen width and height
+        screen_x = self.winfo_screenwidth()
+        screen_y= self.winfo_screenheight()
+
+            # calculate the x and y positions for centering the window
+        x = (screen_x - w) // 2
+        y = (screen_y - h) // 2
+
+        self.geometry(f'{w}x{h}+{x}+{y}')
+
+class MainWindow(Tk):
+
+    ## main window
+    # creating the main window and doing some configuration
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+            # load icon from disk
+        self.iconbitmap("C:/Users/norppa/nerdCorner/PyARR/src/titlebar_icon.ico")
+        self.title("PyARR v0.1.0")
+        self.configure(bg="snow")
+        self.center_window()
+        self.resizable(False, False)
+
+
+            # create frames
+        top_frame = Frame(self, width=580, height=170, bg="PaleVioletRed1")
+        top_frame.grid(row=0, column=0, padx=10, pady=5)
+
+        middle_frame = Frame(self, width=580, height=190, bg="PaleVioletRed1")
+        middle_frame.grid(row=1, column=0, padx=10, pady=5)
+
+        bottom_frame = Frame(self, width=580, height=190, bg="PaleVioletRed1")
+        bottom_frame.grid(row=2, column=0, padx=10, pady=5)
+
+        info_frame = Frame(self, width=580, height=15, bg="purple4")
+        info_frame.grid(row=3, column=0, padx=10, pady=0)
+
+            # create frames and labels inside the first frames
+                                                                        #continue here <--------------
+    def center_window(self):
+        w = 600 # width of the welcome window
+        h = 600 # height of the welcome window
+
+            # get the screen width and height
+        screen_x = self.winfo_screenwidth()
+        screen_y= self.winfo_screenheight()
+
+            # calculate the x and y positions for centering the window
+        x = (screen_x - w) // 2
+        y = (screen_y - h) // 2
+
+        self.geometry(f'{w}x{h}+{x}+{y}')
+
+if __name__ == ("__main__"):
+    main_window = MainWindow()
+    welcome_window = WelcomeWindow(main_window)
+    main_window.mainloop()
