50b06652d57c6845816847c2abf1db6393593c8a
norppa <108995411+norpba@users.noreply.github.com>
2023-12-14 16:43:46 +0200

    Code works now as it is supposed to. Put buttons and most of the functions to their own classes and debugged a ton. Sorting function works as it's supposed to.

--- src/customtkinter_gui.py
+++ src/customtkinter_gui.py
@@ -52,6 +52,7 @@ class MainWindow(customtkinter.CTk):
         self.resizable(False, False)
         self.grid_columnconfigure((0, 1), weight=1)
         WindowUtils.center_window(self)
+        
         self.sourcepath_frame = SourcePathFrame(self)
         self.sourcepath_frame.grid(columnspan=3,
                                    row=1,
@@ -59,13 +60,6 @@ class MainWindow(customtkinter.CTk):
                                    padx=10,
                                    pady=(10, 10),
                                    sticky="nwe")
-        
-        self.sourcebutton_frame = SourceButtonFrame(self.sourcepath_frame, self)
-        self.sourcebutton_frame.grid(row=0,
-                                     column=0,
-                                     padx=10,
-                                     pady=(10, 10),
-                                     sticky="nw")
         
         self.destinationpath_frame = DestinationPathFrame(self)
         self.destinationpath_frame.grid(columnspan=3,
@@ -75,6 +69,13 @@ class MainWindow(customtkinter.CTk):
                                         pady=(10, 10),
                                         sticky="nwe")
         
+        self.sourcebutton_frame = SourceButtonFrame(self.sourcepath_frame, self)
+        self.sourcebutton_frame.grid(row=0,
+                                     column=0,
+                                     padx=10,
+                                     pady=(10, 10),
+                                     sticky="nw")
+        
         self.destinationbutton_frame = DestinationButtonFrame(self.destinationpath_frame, self)
         self.destinationbutton_frame.grid(row=0,
                                           column=1,
@@ -82,7 +83,7 @@ class MainWindow(customtkinter.CTk):
                                           pady=(10, 10),
                                           sticky="nw")
         
-        self.sortingbutton_frame = SortButtonFrame(self)
+        self.sortingbutton_frame = SortButtonFrame(self, self.sourcebutton_frame, self.destinationbutton_frame)
         self.sortingbutton_frame.grid(row=0,
                                       column=2,
                                       padx=10,
@@ -94,8 +95,8 @@ class SourceButtonFrame(customtkinter.CTkFrame):
         super().__init__(*args, **kwargs)
         
         self.source_path_frame = source_path_frame
-        self.source_button = customtkinter.CTkButton(self, text="Select a folder to sort",
-                                                     command=self.SourceFolder)
+        
+        self.source_button = customtkinter.CTkButton(self, text="Select a folder to sort", command=self.SourceFolder)
         self.source_button.grid(row=0, column=0, padx=10, pady=(10, 10))
         
     def SourceFolder(self):
@@ -107,8 +108,8 @@ class DestinationButtonFrame(customtkinter.CTkFrame):
         super().__init__(*args, **kwargs)
         
         self.destination_path_frame = destination_path_frame
-        self.destination_button = customtkinter.CTkButton(self, text="Select an output folder",
-                                                          command=self.DestinationFolder)
+        
+        self.destination_button = customtkinter.CTkButton(self, text="Select an output folder", command=self.DestinationFolder)
         self.destination_button.grid(row=0, column=1, padx=10, pady=(10, 10))
         
     def DestinationFolder(self):
@@ -119,30 +120,38 @@ class SourcePathFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         
-        self.source_label = customtkinter.CTkLabel(self, text="Source folder path:",
-                                                   wraplength=560)
+        self.source_label = customtkinter.CTkLabel(self, text="Source folder path:", wraplength=560)
         self.source_label.grid(row=0, column=0, padx=20, pady=(10, 10))
 
 class DestinationPathFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         
-        self.destination_label = customtkinter.CTkLabel(self, text="Destination folder path:",
-                                                        wraplength=560)
+        self.destination_label = customtkinter.CTkLabel(self, text="Destination folder path:", wraplength=560)
         self.destination_label.grid(row=0, column=0, padx=20, pady=(10, 10))
 
 class SortButtonFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
+    def __init__(self, master, source_button_frame, destination_button_frame, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        
+        self.source_button_frame = source_button_frame  # Update the attribute name
+        self.destination_button_frame = destination_button_frame
         
         self.sorting_button = customtkinter.CTkButton(self, text="Sort", command=self.run_sorter)
         self.sorting_button.grid(row=0, column=2, padx=10, pady=(10, 10))
     
     def run_sorter(self):
-        if 
-        
-        
-    
+        if hasattr(self.source_button_frame, 'src_directory') and hasattr(self.destination_button_frame, 'dst_directory'):
+            src_directory = self.source_button_frame.src_directory  # update the attribute name
+            dst_directory = self.destination_button_frame.dst_directory # update the attribute name
+            
+            print("Source dir: ", src_directory)
+            print("Destination dir: ", dst_directory)
+            
+            sort_files(src_directory, dst_directory)
+        else:
+            print("Source Directory not set.")
+
 class WindowUtils:
     @staticmethod
     def center_window(window):
@@ -162,5 +171,8 @@ class WindowUtils:
 if __name__ == ("__main__"):
     main_window = MainWindow()
     welcome_window = WelcomeWindow(main_window)
+    sort_button_frame = SortButtonFrame(main_window,
+                                        main_window.sourcebutton_frame,
+                                        main_window.destinationbutton_frame)
     main_window.update_idletasks()
     main_window.mainloop()
